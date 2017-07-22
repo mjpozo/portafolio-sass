f199c7c0c41d41624e91de5e8c5ce4243c6d603f
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-07-22 02:49:03 -0400

    section product casi lista version desktop, falta afinar mil detalles en version mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
+	//navbar
 	$(".button-collapse").sideNav();
+
+	//botones con dropdown
 	$('.dropdown-button').dropdown({hover: true});
+
+	//nav cambia de color cuando se baja la página
+	$(document).scroll(function(){
+		if (document.body.scrollTop > 100){
+			$("nav").addClass("nav-scroll");
+		}
+	})
+
+	//para el carousel
 	$('.slider').slider();
-	$('.parallax').parallax();
+
 });
